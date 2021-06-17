708829b64c7f5802721ab7053aac0b203edc9d73
Konstantin Makhov <k.makhov@redmadrobot.com>
2021-06-17 17:36:39 +0300

    Соединил PinCodeEntryVC с LoginService

--- Source/OmegaBank/Sources/Presentation/Auth/PinCode/View Contorller/PinCodeEntryViewController.swift
+++ Source/OmegaBank/Sources/Presentation/Auth/PinCode/View Contorller/PinCodeEntryViewController.swift
@@ -8,26 +8,47 @@
 
 import UIKit
 
+protocol PinCodeEntryViewControllerDelegate: AnyObject {
+    
+    func pinCodeEntryViewControllerEntered(_ controller: PinCodeEntryViewController)
+    
+    func pinCodeEntryViewControllerDidLogout(_ controller: PinCodeEntryViewController)
+    
+}
+
 final class PinCodeEntryViewController: PinCodeBaseViewController {
     
     // MARK: - Public properties
     
-    // Кол-бэк на вход
-    var onEntry: (() -> Void)?
-    
-    // Кол-бэк на выход
-    var onExitButtonPressed: (() -> Void)?
+    weak var delegate: PinCodeEntryViewControllerDelegate?
     
     // MARK: - Private properties
     
+    private var loginService: LoginService
+    
     private var maxInputAttempts = 3
+    
+    // MARK: - Init
+    
+    init(loginService: LoginService) {
+        self.loginService = loginService
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - VC
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = "Sign In"
         isHiddenAvatarImage = false
+        isExitButtonHidden = false
+        setRightButtonBiometricType()
     }
     
     // MARK: - Private methods
@@ -37,18 +58,46 @@ final class PinCodeEntryViewController: PinCodeBaseViewController {
     }
     
     // Вход по пин-коду
-    private func authoriseByPinCode() {
+    private func authorise(by pinCode: String) {
+        do {
+            try loginService.authorise(by: pinCode)
+            delegate?.pinCodeEntryViewControllerEntered(self)
+        } catch {
+            if maxInputAttempts == 1 {
+                delegate?.pinCodeEntryViewControllerDidLogout(self)
+                return
+            }
+            maxInputAttempts -= 1
+            showError(with: maxInputAttempts)
+        }
 
     }
     
     // Вход по биометрии
-    private func entryBiometry() {
-
+    private func authoriseBiometry() {
+        guard loginService.hasBiometricEntry else { return }
+        try? loginService.authoriseWithBiometry { [weak self] in
+            guard let self = self else { return }
+            self.delegate?.pinCodeEntryViewControllerEntered(self)
+        }
     }
     
     // Установка правой кнопки клавиатуры в зависимости от типа биометрии
     private func setRightButtonBiometricType() {
-
+        guard loginService.hasBiometricEntry else {
+            rightButtonItem = .nothing
+            updateRightButton()
+            return }
+        
+        switch loginService.biometricType {
+        case .faceID:
+            rightButtonItem = .faceID
+        case .touchID:
+            rightButtonItem = .touchID
+        default:
+            rightButtonItem = .nothing
+        }
+        updateRightButton()
     }
     
     // Ввод пин-кода
@@ -56,25 +105,24 @@ final class PinCodeEntryViewController: PinCodeBaseViewController {
         super.pinCodeKeyboardView(keyboard, didSelect: digit)
         
         verifyPinCode { [weak self] in
-            self?.authoriseByPinCode()
+            guard let self = self else { return }
+            self.authorise(by: self.pinCode)
         }
     }
     
     // Сброс пароля
     override func pinCodeKeyboardViewDidSelectLeftButton(_ keyboard: PinCodeKeyboardView) {
         super.pinCodeKeyboardViewDidSelectLeftButton(keyboard)
-        onExitButtonPressed?()
+        delegate?.pinCodeEntryViewControllerDidLogout(self)
     }
     
     // Вход по биометрии
     override func pinCodeKeyboardViewDidSelectRightButton(_ keyboard: PinCodeKeyboardView) {
-        super.pinCodeKeyboardViewDidSelectRightButton(keyboard)
-        
         if !pinCode.isEmpty {
             removeLastDigit()
             updateRightButton()
         } else {
-            entryBiometry()
+            authoriseBiometry()
         }
     }
 }
