cf4b7b27ccf77a1600fefdb97653d9bad3ed6735
Konstantin Makhov <k.makhov@redmadrobot.com>
2021-06-22 19:33:21 +0300

    Добавил privacyVC

---
+++ Source/OmegaBank/Sources/Presentation/Common/View Controller/PrivacyProtectionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrivacyProtectionViewController.swift
+//  OmegaBank
+//
+//  Created by Konsantin Makhov on 22.06.2021.
+//  Copyright © 2021 RedMadRobot. All rights reserved.
+//
+
+import UIKit
+
+final class PrivacyProtectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBlurEffect()
+    }
+    
+    private func setupBlurEffect() {
+        let blurEffect = UIBlurEffect(style: UIBlurEffect.Style.dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        
+        view.addSubview(blurEffectView, activate: [
+            view.topAnchor.constraint(equalTo: blurEffectView.topAnchor),
+            view.trailingAnchor.constraint(equalTo: blurEffectView.trailingAnchor),
+            view.leadingAnchor.constraint(equalTo: blurEffectView.leadingAnchor),
+            view.bottomAnchor.constraint(equalTo: blurEffectView.bottomAnchor)
+        ])
+    }
+}
